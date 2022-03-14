83c56654ca4b82a922d0b4b47fd545cc29a7e88d
luan1510 <92529887+luan1510@users.noreply.github.com>
2022-03-14 23:15:10 +0700

    Delete bi2 directory

--- bi2/mainbt2.js
+++
@@ -1,9 +0,0 @@
-let button = document.querySelectorAll('input[type="button"]')
-
-for (let index = 0; index < button.length; index++) {
-    button[index].addEventListener('click', function() {
-        let parent = this.parentElement.parentElement
-        console.log(parent)
-        parent.remove()
-    })
-}
